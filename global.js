f2e24dad108875a72ec365fe0aeeb007db06bf81
Rahul <rahuldharmaraja0798@gmail.com>
2023-12-13 11:20:49 +0530

    global object and window diff

---
+++ global.js
@@ -0,0 +1,13 @@
+// console.log(global);
+
+// setTimeout(() => {
+//     console.log('To Lap');
+//     clearInterval(int);
+// }, 3000)
+
+// const int = setInterval(() => {
+//     console.log('Phil')
+// }, 2000);
+
+console.log(__dirname);
+console.log(__filename);
