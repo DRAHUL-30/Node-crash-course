31dac54fdc89e7c33cd0f2ff935cb7ce660d83db
Rahul <rahuldharmaraja0798@gmail.com>
2023-12-27 13:20:35 +0530

    Nodejs Sequelize table creation test

--- Backend/Configs/Connection.ts
+++ Backend/Configs/Connection.ts
@@ -1,16 +1,21 @@
+// ./Connection.js
 import { sequelize } from "./sequelize";
-const { DataTypes } = require('sequelize');
-import { userModal } from "../Models/user";
+import { User } from "../Models/user";
 
 export const Connection = async () => {
-    let User:any = null;
     try {
         await sequelize.authenticate();
-        console.log('User authentication successful.');
-        User = userModal(sequelize);
-        await sequelize.sync();
-        console.log(`Table created successfully`, {User});
+        console.log('Connection to the database has been established successfully.');
+
+        // Log the current state of the model
+        console.log(User.describe());
+
+        await User.sync({ force: true });
+        console.log("The table for the User model was just (re)created!");
     } catch (error) {
         console.error('Unable to connect to the database:', error);
+    } finally {
+        // Close the connection when done
+        await sequelize.close();
     }
 };
--- Backend/Models/user.ts
+++ Backend/Models/user.ts
@@ -1,24 +1,26 @@
+import { sequelize } from "../Configs/sequelize";
 const { DataTypes, Model } = require('sequelize');
-// import { sequelize } from "../Configs/sequelize";
 
-class User extends Model { }
+class User extends Model {}
 
-export const userModal = (sequelize:any) => {
-    return User.init({
-        // Model attributes are defined here
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        lastName: {
-            type: DataTypes.STRING
-            // allowNull defaults to true
-        }
-        }, {
-        // Other model options go here
-        sequelize, // We need to pass the connection instance
-        modelName: 'User' // We need to choose the model name
-    });
-}
+User.init({
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  }
+}, {
+  // Other model options go here
+  sequelize, // We need to pass the connection instance
+  modelName: 'User' // We need to choose the model name
+});
 
-// console.log(User === sequelize.models.User);
+User.sync({ alter: true });
+
+export { User };
+
+
+
