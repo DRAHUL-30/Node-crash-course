97aebd73c4567a722ee8262a0fa08e5b522e3601
Rahul <rahuldharmaraja0798@gmail.com>
2024-01-09 12:57:24 +0530

    Render DB setup

--- Backend/Configs/config.ts
+++ Backend/Configs/config.ts
@@ -8,6 +8,12 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false, // You may need to set this to false in development
+        // Other SSL options if needed
+      },
+    },
   },
   // Add other environments (production, testing) if needed
 };
